2eef0f1f1547453b73f79bb2da68516ef1336927
Scott Weinstein <sweinstein27@gmail.com>
2018-08-06 18:05:50 +0000

    automatically backed up by learn

--- db/migrate/20180806180454_create_dogs.rb
+++ db/migrate/20180806180454_create_dogs.rb
@@ -1,7 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
   def up
-  end
- 
-  def down
-  end
+      create_table :dogs do |t|
+        t.string :name
+        t.string :breed
+      end
+    end
+   
+    def down
+      drop_table :dogs
+    end
 end
